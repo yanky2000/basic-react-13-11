8036635137524ac399ac9fd2303f1a8b8c4854b9
Victor Yang <yanky2000@yahoo.com>
2017-11-18 02:08:35 +0300

    git cache cleared

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import {render} from 'react-dom'
-import App from './components/App'
-import articles from './fixtures'
-
-render(<App articles = {articles} />, document.getElementById('container'))
