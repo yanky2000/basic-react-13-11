acb1848ff907726ed425c112a9f6bcb58f8acd1a
Victor Yang <yanky2000@yahoo.com>
2017-12-13 19:44:54 +0300

    add paginator obj

--- src/reducer/utils.js
+++ src/reducer/utils.js
@@ -10,3 +10,19 @@ export function arrToMap(arr) {
 export function arrToImmutableMap(arr, RecordModel) {
     return arr.reduce((acc, item) => acc.set(item.id, RecordModel ? new RecordModel(item) : item), new OrderedMap())
 }
+
+
+export function paginatorSet({total, limit, offset}) {
+    const counter = Math.ceil(total / limit)
+    const pageSet = {}
+    let i = 0
+    if (!offset || offset > limit) {
+        offset = limit
+    }
+    for (let x = 1; x <= counter; x++) {
+        pageSet[x] = i
+        i = i + offset
+    }
+    console.log(pageSet[2])
+    return pageSet
+}
